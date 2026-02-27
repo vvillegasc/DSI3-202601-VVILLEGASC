8a426e61bef2f8e1f29557ecf986bac4d87249f8
Valentina Villegas <valentinavillegasc@hotmail.com>
2026-02-26 21:17:00 -0500

    feat(practica):creacion de archivo JS  para variacion de tipos de variables

---
+++ javascript/types.js
@@ -0,0 +1,11 @@
+const nombre = "Valentina Villegas";
+const numero = 1000698702;
+const soyEstudiante = true;
+let sinDefinicion;
+const nula = null;
+
+console.log(nombre);
+console.log(numero);
+console.log(soyEstudiante);
+console.log(sinDefinicion);
+console.log(nula);
