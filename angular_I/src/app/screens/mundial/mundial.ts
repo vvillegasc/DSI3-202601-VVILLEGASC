46f3902de1c79f5e701f4bf84b91c7d43df9940d
Valentina Villegas <valentinavillegasc@hotmail.com>
2026-03-19 21:23:55 -0500

    style(mundial): add comprehensive styling and interactive match calculator

--- angular_I/src/app/screens/mundial/mundial.ts
+++ angular_I/src/app/screens/mundial/mundial.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mundial',
-  imports: [Header],
+  imports: [Header, FormsModule],
   templateUrl: './mundial.html',
   styleUrl: './mundial.css',
 })
-export class Mundial {}
+export class Mundial {
+  casos: number = 0;
+  partidos: { brasil: number; colombia: number; resultado: string }[] = [];
+
+  generateCasos(event: number) {
+    this.casos = event;
+    this.partidos = Array(event)
+      .fill(null)
+      .map(() => ({ brasil: null, colombia: null, resultado: '' }));
+  }
+
+  calcular() {
+    for (let partido of this.partidos) {
+      if (partido.colombia > partido.brasil) {
+        partido.resultado = 'ganamos';
+      } else if (partido.brasil > partido.colombia) {
+        partido.resultado = 'perdimos';
+      } else {
+        partido.resultado = 'casi ganamos';
+      }
+    }
+  }
+}
