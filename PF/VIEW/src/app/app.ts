7e086db5fbb6a202c73e4eac4da0e85175d4c045
Valentina Villegas <valentinavillegasc@hotmail.com>
2026-05-28 09:26:21 -0500

    feat: update Angular configuration and add routing for authentication

--- PF/VIEW/src/app/app.ts
+++ PF/VIEW/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Toast } from './components/organisms/toast/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toast],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('VIEW');
-}
+export class App {}
